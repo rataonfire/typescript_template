de409d4366879f4ce98b695465df6959f1546b62
rat101 <rataonfire@proton.me>
2025-08-04 16:45:51 +0300

    feat:add bash script for creating typescript+eslint+prettier setup

---
+++ setup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+npm init -y
+npm install --save-dev typescript@5.8.3 eslint prettier @typescript-eslint/parser@latest @typescript-eslint/eslint-plugin@latest eslint-config-google eslint-config-prettier eslint-plugin-prettier
+echo "{
+  \"extends\": \"@tsconfig/recommended/tsconfig.json\",
+  \"compilerOptions\": {
+    \"target\": \"ES2022\",
+    \"module\": \"ESNext\",
+    \"moduleResolution\": \"bundler\",
+    \"allowImportingTsExtensions\": true,
+    \"noEmit\": true,
+    \"strict\": true,
+    \"skipLibCheck\": true,
+    \"forceConsistentCasingInFileNames\": true
+  },
+  \"include\": [\"src/**/*\"],
+  \"exclude\": [\"node_modules\", \"dist\"]
+}" >tsconfig.json
+echo "{
+  \"extends\": [
+    \"google\",
+    \"@typescript-eslint/recommended\",
+    \"@typescript-eslint/recommended-requiring-type-checking\",
+    \"prettier\"
+  ],
+  \"parser\": \"@typescript-eslint/parser\",
+  \"parserOptions\": {
+    \"ecmaVersion\": \"latest\",
+    \"sourceType\": \"module\",
+    \"project\": \"./tsconfig.json\"
+  },
+  \"plugins\": [\"@typescript-eslint\", \"prettier\"],
+  \"rules\": {
+    \"prettier/prettier\": \"error\",
+    \"@typescript-eslint/no-unused-vars\": \"error\",
+    \"require-jsdoc\": \"off\",
+    \"valid-jsdoc\": \"off\"
+  },
+  \"env\": {
+    \"node\": true,
+    \"es2022\": true
+  }
+}" >.eslintrc.json
+echo "{
+  \"semi\": true,
+  \"singleQuote\": true,
+  \"tabWidth\": 2,
+  \"trailingComma\": \"es5\",
+  \"bracketSpacing\": false,
+  \"arrowParens\": \"avoid\"
+}" >.prettierrc.json
+new_scripts='{
+  "type-check": "tsc --noEmit",
+  "lint": "eslint . --ext .ts,.tsx",
+  "lint:fix": "eslint . --ext .ts,.tsx --fix",
+  "format": "prettier --write \"**/*.{ts,tsx,js,jsx,json,md}\"",
+  "format:check": "prettier --check \"**/*.{ts,tsx,js,jsx,json,md}\"",
+  "check-all": "npm run type-check && npm run lint && npm run format:check",
+  "fix-all": "npm run format && npm run lint:fix && npm run type-check",
+  "pre-commit": "npm run fix-all",
+  "ci": "npm run check-all"
+}'
+
+# Use jq to set the scripts field
+jq --argjson new_scripts "$new_scripts" '.scripts = $new_scripts' package.json >tmp.json && mv tmp.json package.json
